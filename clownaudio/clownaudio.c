510f54f62aeb101b098ccde8d6433c490c720442
Clownacy <Clownacy@users.noreply.github.com>
2020-02-23 22:14:18 +0000

    Gave the library its own build system
    Can now be used as a CMake subproject. With this, clownaudio can also
    be compiled to a DLL, or a Shared Object.

--- clownaudio/clownaudio.c
+++ clownaudio/clownaudio.c
@@ -18,7 +18,7 @@ static void CallbackStream(void *user_data, float *output_buffer, size_t frames_
 	Mixer_MixSamples(output_buffer, frames_to_do);
 }
 
-bool ClownAudio_Init(void)
+DLL_API bool ClownAudio_Init(void)
 {
 	bool success = false;
 
@@ -44,94 +44,94 @@ bool ClownAudio_Init(void)
 	return success;
 }
 
-void ClownAudio_Deinit(void)
+DLL_API void ClownAudio_Deinit(void)
 {
 	Backend_DestroyStream(stream);
 	Backend_Deinit();
 	Mixer_Deinit();
 }
 
-void ClownAudio_Pause(void)
+DLL_API void ClownAudio_Pause(void)
 {
 	Backend_PauseStream(stream);
 }
 
-void ClownAudio_Unpause(void)
+DLL_API void ClownAudio_Unpause(void)
 {
 	Backend_ResumeStream(stream);
 }
 
-ClownAudio_SoundData* ClownAudio_LoadSoundData(const unsigned char *file_buffer1, size_t file_size1, const unsigned char *file_buffer2, size_t file_size2, bool predecode)
+DLL_API ClownAudio_SoundData* ClownAudio_LoadSoundData(const unsigned char *file_buffer1, size_t file_size1, const unsigned char *file_buffer2, size_t file_size2, bool predecode)
 {
 	return (ClownAudio_SoundData*)Mixer_LoadSoundData(file_buffer1, file_size1, file_buffer2, file_size2, predecode);
 }
 
-void ClownAudio_UnloadSoundData(ClownAudio_SoundData *sound)
+DLL_API void ClownAudio_UnloadSoundData(ClownAudio_SoundData *sound)
 {
 	Mixer_UnloadSoundData((Mixer_SoundData*)sound);
 }
 
-ClownAudio_Sound ClownAudio_CreateSound(ClownAudio_SoundData *sound, bool loop, bool free_when_done)
+DLL_API ClownAudio_Sound ClownAudio_CreateSound(ClownAudio_SoundData *sound, bool loop, bool free_when_done)
 {
 	return Mixer_CreateSound((Mixer_SoundData*)sound, loop, free_when_done);
 }
 
-void ClownAudio_DestroySound(ClownAudio_Sound instance)
+DLL_API void ClownAudio_DestroySound(ClownAudio_Sound instance)
 {
 	Mixer_DestroySound(instance);
 }
 
-void ClownAudio_RewindSound(ClownAudio_Sound instance)
+DLL_API void ClownAudio_RewindSound(ClownAudio_Sound instance)
 {
 	Mixer_RewindSound(instance);
 }
 
-void ClownAudio_PauseSound(ClownAudio_Sound instance)
+DLL_API void ClownAudio_PauseSound(ClownAudio_Sound instance)
 {
 	Mixer_PauseSound(instance);
 }
 
-void ClownAudio_UnpauseSound(ClownAudio_Sound instance)
+DLL_API void ClownAudio_UnpauseSound(ClownAudio_Sound instance)
 {
 	Mixer_UnpauseSound(instance);
 }
 
-void ClownAudio_FadeOutSound(ClownAudio_Sound instance, unsigned int duration)
+DLL_API void ClownAudio_FadeOutSound(ClownAudio_Sound instance, unsigned int duration)
 {
 	Mixer_FadeOutSound(instance, duration);
 }
 
-void ClownAudio_FadeInSound(ClownAudio_Sound instance, unsigned int duration)
+DLL_API void ClownAudio_FadeInSound(ClownAudio_Sound instance, unsigned int duration)
 {
 	Mixer_FadeInSound(instance, duration);
 }
 
-void ClownAudio_CancelFade(ClownAudio_Sound instance)
+DLL_API void ClownAudio_CancelFade(ClownAudio_Sound instance)
 {
 	Mixer_CancelFade(instance);
 }
 
-int ClownAudio_GetSoundStatus(ClownAudio_Sound instance)
+DLL_API int ClownAudio_GetSoundStatus(ClownAudio_Sound instance)
 {
 	return Mixer_GetSoundStatus(instance);
 }
 
-void ClownAudio_SetSoundVolume(ClownAudio_Sound instance, float volume)
+DLL_API void ClownAudio_SetSoundVolume(ClownAudio_Sound instance, float volume)
 {
 	Mixer_SetSoundVolume(instance, volume);
 }
 
-void ClownAudio_SetSoundLoop(ClownAudio_Sound instance, bool loop)
+DLL_API void ClownAudio_SetSoundLoop(ClownAudio_Sound instance, bool loop)
 {
 	Mixer_SetSoundLoop(instance, loop);
 }
 
-void ClownAudio_SetSoundSampleRate(ClownAudio_Sound instance, unsigned long sample_rate1, unsigned long sample_rate2)
+DLL_API void ClownAudio_SetSoundSampleRate(ClownAudio_Sound instance, unsigned long sample_rate1, unsigned long sample_rate2)
 {
 	Mixer_SetSoundSampleRate(instance, sample_rate1, sample_rate2);
 }
 
-void ClownAudio_SetSoundPan(ClownAudio_Sound instance, float pan)
+DLL_API void ClownAudio_SetSoundPan(ClownAudio_Sound instance, float pan)
 {
 	Mixer_SetPan(instance, pan);
 }
--- clownaudio/clownaudio.h
+++ clownaudio/clownaudio.h
@@ -3,6 +3,12 @@
 #include <stdbool.h>
 #include <stddef.h>
 
+#ifdef BUILDING_DLL
+#define DLL_API __declspec(dllexport)
+#else
+#define DLL_API
+#endif
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -10,35 +16,35 @@ extern "C" {
 typedef struct ClownAudio_SoundData ClownAudio_SoundData;
 typedef unsigned int ClownAudio_Sound;
 
-bool ClownAudio_Init(void);
-void ClownAudio_Deinit(void);
+DLL_API bool ClownAudio_Init(void);
+DLL_API void ClownAudio_Deinit(void);
 
-void ClownAudio_Pause(void);
-void ClownAudio_Unpause(void);
+DLL_API void ClownAudio_Pause(void);
+DLL_API void ClownAudio_Unpause(void);
 
-ClownAudio_SoundData* ClownAudio_LoadSoundData(const unsigned char *file_buffer1, size_t file_size1, const unsigned char *file_buffer2, size_t file_size2, bool predecode);
-void ClownAudio_UnloadSoundData(ClownAudio_SoundData *sound);
+DLL_API ClownAudio_SoundData* ClownAudio_LoadSoundData(const unsigned char *file_buffer1, size_t file_size1, const unsigned char *file_buffer2, size_t file_size2, bool predecode);
+DLL_API void ClownAudio_UnloadSoundData(ClownAudio_SoundData *sound);
 
 // If `free_when_done` is true, the sound will be destroyed once it finishes playing
-ClownAudio_Sound ClownAudio_CreateSound(ClownAudio_SoundData *sound, bool loop, bool free_when_done);
-void ClownAudio_DestroySound(ClownAudio_Sound instance);
+DLL_API ClownAudio_Sound ClownAudio_CreateSound(ClownAudio_SoundData *sound, bool loop, bool free_when_done);
+DLL_API void ClownAudio_DestroySound(ClownAudio_Sound instance);
 
-void ClownAudio_RewindSound(ClownAudio_Sound instance);
+DLL_API void ClownAudio_RewindSound(ClownAudio_Sound instance);
 
-void ClownAudio_PauseSound(ClownAudio_Sound instance);
-void ClownAudio_UnpauseSound(ClownAudio_Sound instance);
+DLL_API void ClownAudio_PauseSound(ClownAudio_Sound instance);
+DLL_API void ClownAudio_UnpauseSound(ClownAudio_Sound instance);
 
-void ClownAudio_FadeOutSound(ClownAudio_Sound instance, unsigned int duration);	// Duration is in milliseconds
-void ClownAudio_FadeInSound(ClownAudio_Sound instance, unsigned int duration);
-void ClownAudio_CancelFade(ClownAudio_Sound instance);
+DLL_API void ClownAudio_FadeOutSound(ClownAudio_Sound instance, unsigned int duration);	// Duration is in milliseconds
+DLL_API void ClownAudio_FadeInSound(ClownAudio_Sound instance, unsigned int duration);
+DLL_API void ClownAudio_CancelFade(ClownAudio_Sound instance);
 
 // Returns -1 if the sound doesn't exist, 0 if it's unpaused, or 1 if it is paused
-int ClownAudio_GetSoundStatus(ClownAudio_Sound instance);
+DLL_API int ClownAudio_GetSoundStatus(ClownAudio_Sound instance);
 
-void ClownAudio_SetSoundVolume(ClownAudio_Sound instance, float volume);	// Volume is logarithmic, between 0.0f and 1.0f
-void ClownAudio_SetSoundLoop(ClownAudio_Sound instance, bool loop);
-void ClownAudio_SetSoundSampleRate(ClownAudio_Sound instance, unsigned long sample_rate1, unsigned long sample_rate2);
-void ClownAudio_SetSoundPan(ClownAudio_Sound instance, float pan);	// -1.0f is full-left; 1.0f is full-right
+DLL_API void ClownAudio_SetSoundVolume(ClownAudio_Sound instance, float volume);	// Volume is logarithmic, between 0.0f and 1.0f
+DLL_API void ClownAudio_SetSoundLoop(ClownAudio_Sound instance, bool loop);
+DLL_API void ClownAudio_SetSoundSampleRate(ClownAudio_Sound instance, unsigned long sample_rate1, unsigned long sample_rate2);
+DLL_API void ClownAudio_SetSoundPan(ClownAudio_Sound instance, float pan);	// -1.0f is full-left; 1.0f is full-right
 
 #ifdef __cplusplus
 }
