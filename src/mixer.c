966a66c0f88e11a10775f5a00e3c1ff59228f9e8
Clownacy <Clownacy@users.noreply.github.com>
2021-05-31 19:58:22 +0100

    Add a small optimisation
    Brownbot and godbolt show that compilers aren't smart enough to
    automatically perform this optimisation.

--- src/mixer.c
+++ src/mixer.c
@@ -226,8 +226,8 @@ CLOWNAUDIO_EXPORT ClownAudio_Mixer* ClownAudio_Mixer_Create(unsigned long sample
 
 	if (mixer != NULL)
 	{
-		for (size_t i = 0; i < COUNT_OF(mixer->sound_hash_table); ++i)
-			mixer->sound_hash_table[i] = NULL;
+		// Set all bucket pointers to NULL
+		memset(mixer->sound_hash_table, 0, sizeof(mixer->sound_hash_table));
 
 		mixer->playing_list_head = NULL;
 
