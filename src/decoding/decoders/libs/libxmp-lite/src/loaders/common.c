1d1d4d821f81e19eae729d0d915141e123b11236
Clownacy <Clownacy@users.noreply.github.com>
2021-07-04 12:02:51 +0100

    Fix libxmp-lite to use relative include paths
    I'd contribute this fix back upstream, but I imagine there's some
    goofy build-system-related reason libxmp-lite uses absolute paths.

--- src/decoding/decoders/libs/libxmp-lite/src/loaders/common.c
+++ src/decoding/decoders/libs/libxmp-lite/src/loaders/common.c
@@ -42,8 +42,8 @@
 #endif /* LIBXMP_CORE_PLAYER */
 
 #include "xmp.h"
-#include "common.h"
-#include "period.h"
+#include "../common.h"
+#include "../period.h"
 #include "loader.h"
 
 int libxmp_init_instrument(struct module_data *m)
