8ac18f798b377685274a9c8e50dc4f919244a518
Clownacy <Clownacy@users.noreply.github.com>
2021-07-13 23:22:22 +0100

    Don't include miniaudio.h without its defines
    Including the same single-header-library twice with different defines
    can cause errors.

--- src/playback/miniaudio.c
+++ src/playback/miniaudio.c
@@ -24,6 +24,12 @@
 #include <stddef.h>
 #include <stdlib.h>
 
+#define MA_NO_DECODING
+#define MA_NO_ENCODING
+#define MA_NO_WAV
+#define MA_NO_FLAC
+#define MA_NO_MP3
+#define MA_NO_GENERATION
 #include "../miniaudio.h"
 
 struct ClownAudio_Stream
