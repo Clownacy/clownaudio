3adcbdf44c61461fffd4ec9d8a94ea563ce3cc4c
Clownacy <Clownacy@users.noreply.github.com>
2021-09-14 11:38:11 +0100

    Make the CLAMP macro less ugly

--- src/mixer.c
+++ src/mixer.c
@@ -36,7 +36,7 @@
 
 #define MIN(a, b) ((a) < (b) ? (a) : (b))
 #define MAX(a, b) ((a) > (b) ? (a) : (b))
-#define CLAMP(x, min, max) MIN(MAX((x), (min)), (max))
+#define CLAMP(x, min, max) (MIN((max), MAX((min), (x))))
 
 #define SCALE(x, scale) (((x) * (scale)) / 0x100)
 
