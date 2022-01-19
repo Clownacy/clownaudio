15fb77f13785543f810e4421a2585191509e2255
Clownacy <Clownacy@users.noreply.github.com>
2022-01-19 18:50:40 +0000

    Disable miniaudio stuff that we don't need

--- src/miniaudio.c
+++ src/miniaudio.c
@@ -5,7 +5,10 @@
 #define MA_NO_WAV
 #define MA_NO_FLAC
 #define MA_NO_MP3
+#define MA_NO_VORBIS
 #define MA_NO_GENERATION
+#define MA_NO_ENGINE
+#define MA_NO_NODE_GRAPH
 
 #ifndef CLOWNAUDIO_MINIAUDIO_ENABLE_DEVICE_IO
  #define MA_NO_DEVICE_IO
